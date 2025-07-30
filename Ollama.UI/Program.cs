b0eaa1069ed357c4e32ce297715d73db8ef57847
Gabriel Beserra <gabriel.fernandesb@gmail.com>
2025-07-30 18:58:44 -0300

    feat: add user interface

---
+++ Ollama.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ollama.Application.Interfaces;
+using Ollama.Application.IoC;
+using Ollama.Infrastructure.IoC;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+var app = builder.Build();
+
+var chatService = app.Services.GetRequiredService<IChat>();
+chatService.GreetUser();
+
+while (true)
+{
+    var prompt = Console.ReadLine();
+    await chatService.GetUserPrompt(prompt);
+    Console.WriteLine();
+    Console.WriteLine();
+}
